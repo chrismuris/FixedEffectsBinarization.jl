7b9d032c67e7ccc56483f35fc457369c6dd6f25a
Chris Muris <cmuris@sfu.ca>
2018-02-24 10:05:49 +0000

    Moved explanation on structs to strucs.jl.

--- src/FixedEffectsBinarization.jl
+++ src/FixedEffectsBinarization.jl
@@ -12,13 +12,4 @@ include("datahandling.jl")
 include("estimation.jl")
 include("postestimation.jl")
 
-# Each application is characterized by:
-# 1. (y,X) (well, formula, data, :i, : for the user)
-# 2. Which cut points to choose
-# 3. How to interpolate the estimated function in between the cut points.
-# If the outcome variable is discrete, then generally 2. will correspond to all points in the support of the dependent variable, and 3. will be a step function.
-# If the outcome variable is continuous, then generally 2. will consist of some cleverly chosen points, and 3. will be a linear interpolation, or a B-spline, or something like that. I like linear interpolation because it is consistent with censoring.
-
-
-
 end
--- src/strucs.jl
+++ src/strucs.jl
@@ -1,5 +1,11 @@
 export FELT, newFELT
 
+# Each application is characterized by:
+# 1. (y,X) (well, formula, data, :i, : for the user)
+# 2. Which cut points to use
+# 3. How to interpolate the estimated function in between the cut points.
+# If the outcome variable is discrete, then generally 2. will correspond to all points in the support of the dependent variable, and 3. will be a step function.
+# If the outcome variable is continuous, then generally 2. will consist of some cleverly chosen points, and 3. will be a linear interpolation, or a B-spline, or something like that. I like linear interpolation because it is consistent with censoring.
 mutable struct FELT
     formula
     data::DataFrame
